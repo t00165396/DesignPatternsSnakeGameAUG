dcf9f4c126640e2fb97679291a4e2370c48aa966
t00165396 <liranda.krasniqi@ittralee.ie>
2017-07-29 12:13:14 +0100

    state design pattern

---
+++ SnakeG/src/snakeg/Right.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package snakeg;
+
+/**
+ *
+ * @author Liranda Krasniqi
+ */
+public class Right implements snakeDirection
+{
+    private static int DX = 1;
+    private static int DY = 0;
+    public int getDX()
+    {
+        return DX;
+    }
+    public int getDY()
+    {
+        return DY;
+    }
+    
+}
