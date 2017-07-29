483c47c06768b7cbd9f92c60a3d94636c3ef7c2d
t00165396 <liranda.krasniqi@ittralee.ie>
2017-07-29 12:13:05 +0100

    state design pattern

---
+++ SnakeG/src/snakeg/Up.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package snakeg;
+/**
+ *
+ * @author Liranda Krasniqi
+ */
+public class Up implements snakeDirection
+{
+    private static int DX = 0;
+    private static int DY = -1;
+    public int getDX()
+    {
+        return DX;
+    }
+    public int getDY()
+    {
+        return DY;
+    }
+    
+}
